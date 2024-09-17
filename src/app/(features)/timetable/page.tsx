cd2f70227456edc884ccece490d92306b3e318e0
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2024-09-17 14:33:51 +0800

    Added ModuleList

--- src/app/(features)/timetable/page.tsx
+++ src/app/(features)/timetable/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import { ModuleTreeComponent } from "@/components/ModuleTree";
+import PrerequisiteStatusList from "@/components/PreRequisiteList";
+import { useModuleStore } from "@/stores/StoreProviders";
+import type { Module } from "@/types/primitives/module";
+import { useEffect, useState } from "react";
+
 export default function TimeTablePage() {
-  return <h1>TimeTablePage</h1>;
+  const { getModule } = useModuleStore((state) => state);
+
+  const [module, setModule] = useState<Module>();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getModule("IS216")
+      .catch((e) => console.log(e))
+      .then((mod) => {
+        if (!mod) return;
+        setModule(mod);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div>
+      {!loading ? (
+        !!module ? (
+          <>
+            <ModuleTreeComponent
+              moduleCode={module.moduleCode}
+              prereqTree={module.preReq}
+            />
+            <PrerequisiteStatusList
+              preReqTree={module.preReq}
+              completedModules={["IS113"]}
+            />
+          </>
+        ) : (
+          <p>Module not found</p>
+        )
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
