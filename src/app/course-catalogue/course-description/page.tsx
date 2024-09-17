1158c4ca7649a481d82197c0dd2550fd2cc393c8
arinmakk <arinmakk@gmail.com>
2024-09-17 16:54:36 +0800

    hhh

--- src/app/course-catalogue/course-description/page.tsx
+++ src/app/course-catalogue/course-description/page.tsx
@@ -1,3 +1,13 @@
-export default function courseCatalogueNested () {
-    return <h1>Course description nested</h1>;
+// page.tsx
+import { modules } from '@/server/data/modules'; // Import the `modules` object
+
+export default function CourseCatalogueNested() {
+    return (
+        <div>
+            <h1>Course description nested</h1>
+            {Object.values(modules).map((course) => (
+                <h1 key={course.moduleCode}>{course.name}</h1> // Display the course name
+            ))}
+        </div>
+    );
 }
