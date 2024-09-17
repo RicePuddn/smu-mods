93f9f1313d2b9f65ef0eee124fa259b625dc7dc2
Isaiah Chia <130481550+Isaidchia@users.noreply.github.com>
2024-09-17 21:30:08 +0800

    Populated Club Cards w Mock Data

--- src/app/(features)/beyond-studies/page.tsx
+++ src/app/(features)/beyond-studies/page.tsx
@@ -1,27 +1,44 @@
 "use client";
 
-import { Card, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { clubs } from "@/server/data/club-board";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function BeyondStudies() {
   const [item, setItem] = useState("");
+  const [clubData, setclubData] = useState(clubs);
+  //   console.log(clubData);
 
   return (
     <div className="mx-auto text-center md:container">
       <h1 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
         Beyond Studies
       </h1>
-      <div className="grid grid-cols-4 gap-4 rounded border p-12">
-        <Card>
-          <CardHeader>Hello</CardHeader>
-        </Card>
-        <div className="rounded border p-8">Placeholder</div>
-        <div className="rounded border p-8">Placeholder</div>
-        <div className="rounded border p-8">Placeholder</div>
-        <div className="rounded border p-8">Placeholder</div>
-        <div className="rounded border p-8">Placeholder</div>
-        <div className="rounded border p-8">Placeholder</div>
-        <div className="rounded border p-8">Placeholder</div>
+      <div className="grid grid-cols-3 gap-4 rounded p-12">
+        {Object.entries(clubData).map(([key, value]) => (
+          <Card className="p-6">
+            <Image
+              className="rounded-t-lg bg-gray-400"
+              src="/ellipsis_badge.png"
+              alt="logo"
+              width={500}
+              height={500}
+            />
+            <CardHeader>{value.name}</CardHeader>
+            <CardDescription className="text-left">
+              {value.desc}
+            </CardDescription>
+            <CardFooter className="pt-2 text-center">
+              {value.contact}
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </div>
   );
--- src/server/data/club-board.ts
+++ src/server/data/club-board.ts
@@ -1,18 +1,18 @@
 import { Clubs } from "@/types/primitives/club";
 
 export const clubs: Clubs ={
-    ellipsis : {   name: "Ellipsis",
+    0 : {   name: "Ellipsis",
         desc: "SMU Computing & Information Systems Society, affectionately known as Ellipsis, is the student representative body of the School of Computing & Information Systems (SCIS).",
         contact: "@Felicia",
         events: ["TechSeries", "Meta Camp", "Welfare Drives"]
     },
-    smubia : {
+    1 : {
         name: "Business Intelligence & Analytics",
         desc: "SMUBIA provides a space for connecting students, alumni and institutions who are passionate about data analytics and business intelligence. As we integrate ourselves into the Information Age, data analytics has become an indispensable tool in our lives.",
         contact: "@Zack",
         events: ["Data Associate Programme", "Technical Workshops", "Welfare Drives"]
     },
-    smudothack : {
+    2 : {
         name: ".Hack",
         desc: "SMU .Hack is a special interest group with a deep passion for tech, with a strong focus on software engineering and web development. With many new technologies, the bread and butter for human interaction with the digital world lies in well built, robust and scalable software systems, many of which reside on the web. Exploring the limits of software development can be daunting, therefore at SMU .Hack, we come together as a community of like-minded tech enthusiasts, sharing our experiences and knowledge as a tightly knitted group.",
         contact: "@Jack",
