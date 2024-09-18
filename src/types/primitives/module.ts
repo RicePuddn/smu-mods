4b273da448c2dbe24ed76b4612fd58d3a66ebe17
arinmakk <arinmakk@gmail.com>
2024-09-19 00:39:50 +0800

    data

--- src/types/primitives/module.ts
+++ src/types/primitives/module.ts
@@ -25,16 +25,16 @@ export const terms = ["Term 1", "Term 2", "Term 3A", "Term 3B"] as const;
 export type Term = (typeof terms)[number];
 
 export const startingTime = [
-  "8:15",
+  "08:15",
   "10:00",
   "12:00",
   "13:45",
-  "3:30",
-  "5:15",
-  "7:00",
-  "8:45",
+  "15:30",
+  "17:15",
+  "19:00",
+  "20:45",
 ] as const;
-export const duration = [1.5, 3] as const;
+export const duration = [1.5, 3.25] as const;
 
 export type StartingTime = (typeof startingTime)[number];
 export type Duration = (typeof duration)[number];
