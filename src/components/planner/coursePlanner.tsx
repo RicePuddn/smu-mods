069d91b717d10352a632f4838ed94804cf90a874
Felicia Tanujaya <148226847+FeliciaaaTan@users.noreply.github.com>
2024-10-18 01:14:29 +0800

    Added search suggestion, hide special term, and horizontal scrolling

--- src/components/planner/coursePlanner.tsx
+++ src/components/planner/coursePlanner.tsx
@@ -13,8 +13,9 @@ import {
   Droppable,
   type DropResult,
 } from "@hello-pangea/dnd";
+import { debounce } from "lodash";
 import { ChevronDown, ChevronUp, X } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import "./scrollBar.css";
@@ -22,16 +23,47 @@ import "./scrollBar.css";
 const DELIMITER = "/$/"
 
 const CoursePlanner: React.FC = () => {
-  const {mutateAsync} = api.module.searchModule.useMutation({
-    onSuccess: (data)=>{
-      setSearchResult(data)
+
+  const {mutateAsync} = api.module.searchModule.useMutation(
+    // {
+    // onSuccess: (data)=>{
+    //   setSuggestionResults(data);
+    //   setShowSuggestion(true);
+    // }
+    // }
+  ) 
+
+  const [searchString, setSearchString] = useState("");
+  const [suggestionResults, setSuggestionResults] = useState<Module[]>([]);
+  const [showSuggestion, setShowSuggestion] = useState<boolean>(false);
+  const [searchResult, setSearchResult] = useState<Module[]>([]);
+  const [showSearchResult, setShowSearchResult]= useState<boolean>(false);
+
+  const debouncedSearch = debounce((query: string) => {
+    if (query.length > 0) {
+      mutateAsync({
+        query: searchString
+      }).then((results) => {
+        setSuggestionResults(results);
+        setShowSuggestion(true);
+      });
+    } else {
+      setShowSuggestion(false);
     }
-  }) 
-  const [searchString, setSearchString] = useState("")
-  const [searchResult, setSearchResult] = useState<Module[]>([])
+  }, 300);
+
+  useEffect(() => {
+    debouncedSearch(searchString);
+  }, [searchString]);
+  useEffect(() => {
+    return () => {
+      debouncedSearch.cancel(); 
+    };
+  }, [])
+
 
   const isMobile = useIsMobile(); 
-  const { addModule: addModuleToPlanner, changeTerm, planner, removeModule } = usePlannerStore((state) => state);
+  const { addModule: addModuleToPlanner, changeTerm, planner, removeModule, hideSpecial } = usePlannerStore((state) => state);
   const { modules, addModule: addModuleToBank } = useModuleBankStore((state) => state);
   const [isOpen, setIsOpen] = React.useState<Set<string>>(new Set())
   
@@ -80,7 +112,12 @@ const CoursePlanner: React.FC = () => {
       }
       return newOpenYears;
     });
+  
   };
+
+  const handleHideSpecial = (year: Year) =>{
+    hideSpecial(year)
+  }
 
   return (
     <div className="p-4">
@@ -88,10 +125,11 @@ const CoursePlanner: React.FC = () => {
         <div className={cn("mb-6", isMobile ? "grid gap-6 grid-cols-1" : "flex flex-nowrap overflow-x-auto scroll-smooth scrollbar-hide")}>
           {Object.keys(planner).sort((a,b) => parseInt(a) - parseInt(b)).map((year) => {
             const terms = planner[year as Year]
+            const isHidden = usePlannerStore((state) => state.isSpecialHidden[year as Year])
             return (
             <div
               key={year}
-              className={cn("overflow-hidden rounded-lg bg-white shadow-md flex flex-col",
+              className={cn("overflow-hidden rounded-lg bg-gray-50 shadow-md flex flex-col",
               !isMobile && "flex-shrink-0 w-96 mr-6 mb-6")}
             >
               <div className={cn("flex justify-between bg-blue-500 p-3 items-center h-14", 
@@ -106,12 +144,29 @@ const CoursePlanner: React.FC = () => {
                       : `Year ${year}`
                     }
                 </h2>
+                {/* {year !== EXEMPTION_YEAR && !isMobile && (
+                <button
+                    className="ml-2 px-3 py-1 bg-white text-blue-500 rounded-md"
+                    onClick={() => handleHideSpecial(year as Year)} 
+                  >
+                    {isHidden ? "Show Special Terms" : "Hide Special Terms"}
+                </button>
+                )} */}
                 {isMobile && (
                   !isMobile || isOpen.has(year)  ? <ChevronUp className="text-white" /> : <ChevronDown className="text-white" />
                 )}
               </div>
               {(!isMobile || isOpen.has(year)) && (
-                Object.entries(terms).map(([term, termModules]) => (
+                <>
+                {year !== EXEMPTION_YEAR &&(
+                  <button
+                      className="px-3 py-1 bg-white text-blue-500 font-medium rounded-md border border-blue-100 shadow-sm my-1 mx-2 hover:bg-gray-100"
+                      onClick={() => handleHideSpecial(year as Year)} 
+                    >
+                      {isHidden ? "Show Special Terms" : "Hide Special Terms"}
+                  </button>
+                  )}
+                {Object.entries(terms).map(([term, termModules]) => (
                   <Droppable
                     droppableId={`${year}${DELIMITER}${term}`}
                     key={`${year}${DELIMITER}${term}`}
@@ -121,7 +176,7 @@ const CoursePlanner: React.FC = () => {
                         <div
                         ref={provided.innerRef}
                         {...provided.droppableProps}
-                        className={cn("p-3 transition-colors duration-200", snapshot.isDraggingOver ? "bg-blue-100" : "bg-gray-50", year === EXEMPTION_YEAR && !isMobile ? "flex-grow" : year === MODSTOTAKE_YEAR && !isMobile ? "flex-grow" : "min-h-[120px]" )}
+                        className={cn("p-3 transition-colors duration-200", snapshot.isDraggingOver ? "bg-blue-100" : "bg-gray-50", term === "Term 3A" && isHidden ? "hidden" : "", term === "Term 3B" && isHidden ? "hidden" : "", year === EXEMPTION_YEAR && !isMobile ? "flex-grow" : year === MODSTOTAKE_YEAR && !isMobile ? "flex-grow" : "min-h-[120px]")}
                         >
                         
                         <h3 className="mb-3 font-medium text-gray-700">
@@ -169,13 +224,15 @@ const CoursePlanner: React.FC = () => {
                       </div>
                     )}
                   </Droppable>
-                ))
+                ))}
+                </>
               )}
             </div>
-          )})}
-        </div>
-      </DragDropContext>
-      <div className="flex items-center">
+          );
+        })}
+      </div>
+    </DragDropContext>
+      <div className="flex">
         <div className="flex-auto max-w-xl">
           <Input
           value={searchString}
@@ -184,13 +241,28 @@ const CoursePlanner: React.FC = () => {
           placeholder="Search Module"
           >
           </Input>
+
+          {showSuggestion && suggestionResults.length > 0 && (
+            <ul className="md absolute mt-1 max-h-40 overflow-auto rounded border border-gray-300 bg-white text-sm shadow-lg">
+              {suggestionResults.map((module, index) => (
+                <li
+                  key={index}
+                  className="cursor-pointer p-2 hover:bg-gray-100"
+                  onClick={()=>HandleAddMod(module)}
+                >
+                  {module.moduleCode} - {module.name}
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
+        
         <div className="ps-4">
           <Button
             onClick={async()=>{
-              const result= await mutateAsync({
-                query: searchString
-              })
+              setSearchResult(suggestionResults)
+              setShowSearchResult(true)
+              setShowSuggestion(false)
             }}
             className="rounded bg-sky-500 px-3 py-2 font-bold text-white transition-colors duration-200 hover:bg-sky-600"
           >
@@ -199,7 +271,7 @@ const CoursePlanner: React.FC = () => {
         </div>
       </div>
       <div className="grid grid-cols-5">
-        {searchResult.length > 0 ?
+        {showSearchResult ? searchResult.length > 0 ?
         (searchResult.map((module, index) => (
           <div key={index} className="col-span-5 p-2 border-b border-gray-200 grid grid-cols-2">
             <div className="col-span-1 py-2">
@@ -216,6 +288,8 @@ const CoursePlanner: React.FC = () => {
           </div>
         ))) : 
         (<div className="p-2"> No Result </div>)
+        :
+        (<div></div>)
         }
       </div>
       {/* <Button
