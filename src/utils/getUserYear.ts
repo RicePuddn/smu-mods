c69636e3f9c217907e23b6bd6697622fc3d14087
Felicia Paulus <feliciapaulus05@gmail.com>
2024-10-22 22:44:24 +0800

    added getUserYear function

---
+++ src/utils/getUserYear.ts
@@ -0,0 +1,20 @@
+import { AcademicYear } from "@/config";
+import { Year, years } from "@/types/planner";
+
+export function getUserYear(matriculationYear:AcademicYear, currentAcademicYear:AcademicYear) : Year {
+    const [startYear, endYear] = currentAcademicYear.split('/').map(Number);
+    const realMatriculationYear = Number(matriculationYear.split('/')[0]);
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth() + 1; // because JavaScript sets 0 as the first month
+    const currentYear = currentDate.getFullYear();
+    let userYear = currentYear - realMatriculationYear + 1;
+    if (currentYear == endYear && currentMonth <= 4) {
+        userYear -= 1;
+    }
+    if (userYear >= 1 && userYear <= parseInt(years.at(-1)!)) {
+        return String(userYear) as Year;
+      } else {
+        console.warn("Invalid user year calculation");
+        return "1"
+      }
+}
