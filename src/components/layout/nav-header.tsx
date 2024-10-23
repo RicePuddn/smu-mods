0856b474be56e92c85b8f1f58cfdbd7e9741efa9
arinmakk <arinmakk@gmail.com>
2024-10-24 00:58:18 +0800

    fixing the logo halfway

--- src/components/layout/nav-header.tsx
+++ src/components/layout/nav-header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import lightModeLogo from './light-mode.png'
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { APP_CONFIG } from "@/config";
 import { termMap } from "@/types/planner";
@@ -18,13 +18,16 @@ export default function NavHeader() {
           priority
         />
       </div> */}
-      <Image
-            src="/light-mode.png"
-            width={500}
-            height={500}
-            alt="Picture of the author"
-      />
-      <h1 className="text-xl font-semibold">SMUMODS</h1>
+      
+      <div className="relative h-12 w-12">
+        <Image
+          src={lightModeLogo}
+          alt="Logo"
+          width={500}
+          height={500}
+        />
+      </div>
+      {/* <h1 className="text-xl font-semibold">SMUMODS</h1> */}
       <p className="ml-auto mr-2 text-sm">
         AY{APP_CONFIG.academicYear}, {termMap[APP_CONFIG.currentTerm]}
       </p>
