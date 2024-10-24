52a7bb1aaf8b04d653cf794c83fb972488901e82
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2024-10-25 01:39:38 +0800

    Fixing V1

---
+++ src/components/acad-clubs/EventCard.tsx
@@ -0,0 +1,49 @@
+import { ReactNode } from "react";
+import { format } from "date-fns";
+
+import { ExtendedSchoolEvent } from "@/stores/event";
+
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+
+interface EventCardProps {
+  event: ExtendedSchoolEvent;
+  actions?: ((
+    event: ExtendedSchoolEvent,
+    index: string | number,
+  ) => ReactNode)[];
+}
+
+export const EventCard = ({ event, actions }: EventCardProps) => {
+  return (
+    <Card className="overflow-hidden bg-accent">
+      {/* Title remains visible */}
+      <CardHeader className="bg-primary p-3 text-center text-lg font-bold text-primary-foreground">
+        {event.title}
+      </CardHeader>
+
+      {/* Content is positioned outside of view and moved up on hover */}
+      <CardContent className="p-4">
+        <p className="text-sm">{event.name}</p>
+        <p>{format(new Date(event.date), "MMMM d, yyyy")}</p>
+        <p className="text-sm">
+          {format(new Date(event.startTime), "HH:mm")} to{" "}
+          {format(new Date(event.endTime), "HH:mm")}
+        </p>
+        <p className="text-sm">{event.venue}</p>
+        <p className="mt-2 text-red-500">
+          <strong>
+            Registration Deadline:{" "}
+            {format(new Date(event.deadline), "MMMM d, yyyy HH:mm")}
+          </strong>
+        </p>
+        {/* <CardDescription className="mt-2 leading-relaxed text-gray-700">
+      <ReadMore id={value.description} text={value.description} />
+    </CardDescription> */}
+      </CardContent>
+
+      <CardFooter className="gap-2">
+        {actions?.map((action, index) => action(event, index))}
+      </CardFooter>
+    </Card>
+  );
+};
--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -1,104 +1,116 @@
-
-import { env } from "@/env";
+import { TRPCError } from "@trpc/server";
 import { OpenAI } from "openai";
 import { z } from "zod";
+
 import { env } from "@/env";
+
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { deleteImage } from "./s3";
 
 // export const openai: OpenAIProvider = createOpenAI({
 //   apiKey: env.OPENAI_API_KEY,
 // });
-export  const openai = new OpenAI({
+export const openai = new OpenAI({
   apiKey: env.OPENAI_API_KEY,
 });
 
 const prompt = ` 
 Based on the this image, extract the following details from the infographic and return it in the JSON format given.`;
 
-
 export const openaiRouter = createTRPCRouter({
-  parseEvent: publicProcedure.input(z.object({
-    srcUrl : z.string().url(),
-  })
-).mutation(async ({input}) => {
-    try {
-      const completion = await openai.chat.completions.create({
-        model: "gpt-4o",
-        messages: [
-          {
-            role: "user",
-            content: [
-              { type: "text", text: prompt },
-              {
-                type: "image_url",
-                image_url: {
-                  url: input.srcUrl,
+  parseEvent: publicProcedure
+    .input(
+      z.object({
+        srcUrl: z.string().url(),
+        key: z.string(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      try {
+        const completion = await openai.chat.completions.create({
+          model: "gpt-4o",
+          messages: [
+            {
+              role: "user",
+              content: [
+                { type: "text", text: prompt },
+                {
+                  type: "image_url",
+                  image_url: {
+                    url: input.srcUrl,
+                  },
                 },
+              ],
+            },
+          ],
+          // schema: z.object({}),
+          // prompt: "",
+          response_format: {
+            // See /docs/guides/structured-outputs
+            type: "json_schema",
+            json_schema: {
+              name: "event_schema",
+              schema: {
+                type: "object",
+                properties: {
+                  name: {
+                    description: "the-club-name",
+                    type: "string",
+                  },
+                  title: {
+                    description: "the-event-title",
+                    type: "string",
+                  },
+                  description: {
+                    description: "description-of-event",
+                    type: "string",
+                  },
+                  date: {
+                    description: "YYYY-MM-DDTHH:MM:SS+08:00",
+                    type: "string",
+                  },
+                  startTime: {
+                    description: "YYYY-MM-DDTHH:MM:SS+08:00",
+                    type: "string",
+                  },
+                  endTime: {
+                    description: "YYYY-MM-DDTHH:MM:SS+08:00",
+                    type: "string",
+                  },
+                  venue: {
+                    description: "the-venue-listed",
+                    type: "string",
+                  },
+                  deadline: {
+                    description: "YYYY-MM-DDTHH:MM:SS+08:00",
+                    type: "string",
+                  },
+                },
+                additionalProperties: false,
               },
-            ],
-          },
-        ],
-        // schema: z.object({}),
-        // prompt: "",
-        response_format: {
-          // See /docs/guides/structured-outputs
-          type: "json_schema",
-          json_schema: {
-            name: "event_schema",
-            schema: {
-              type: "object",
-              properties: {
-                name: {
-                  description: "the-club-name",
-                  type: "string",
-                },
-                title: {
-                  description: "the-event-title",
-                  type: "string",
-                },
-                description: {
-                  description: "description-of-event",
-                  type: "string",
-                },
-                date: {
-                  description: "YYYY-MM-DD",
-                  type: "string",
-                },
-                startTime: {
-                  description: "HH:MM:SS+08:00",
-                  type: "string",
-                },
-                endTime: {
-                  description: "HH:MM:SS+08:00",
-                  type: "string",
-                },
-                venue: {
-                  description: "the-venue-listed",
-                  type: "string",
-                },
-                deadline: {
-                  description: "YYYY-MM-DD",
-                  type: "string",
-                },
-              },
-              additionalProperties: false,
             },
           },
-        },
-      });
-      // console.log(completion.choices[0].finish_reason);
-      // console.log(completion.choices[0].message.content);
-      const content = completion.choices?.[0]?.message?.content;
+        });
+        // console.log(completion.choices[0].finish_reason);
+        // console.log(completion.choices[0].message.content);
+        const content = completion.choices?.[0]?.message?.content;
 
-      if (!content) {
-        throw new Error("No content returned from OpenAI.");
+        if (!content) {
+          throw new TRPCError({
+            message: "No content returned from OpenAI.",
+            code: "BAD_REQUEST",
+          });
+        }
+
+        await deleteImage(input.key);
+
+        return content;
+      } catch (error) {
+        console.error("Error fetching completion:", error);
+        throw new TRPCError({
+          message: "No content returned from OpenAI.",
+          code: "BAD_REQUEST",
+        });
       }
-
-      return content;
-    } catch(error){
-      console.error("Error fetching completion:", error);
-      throw new Error("Failed to parse event information.");
-    }
-    
-  }),
+    }),
 });
