c51739284cdc1d7107ad1056e9661550e7842d2c
arinmakk <arinmakk@gmail.com>
2024-10-27 01:16:51 +0800

    final commit of the day...

--- src/app/(features)/beyond-studies/page.tsx
+++ src/app/(features)/beyond-studies/page.tsx
@@ -43,200 +43,202 @@ export default function BeyondStudies() {
   };
 
   // Function to load an image and convert it to Base64
-  function getImageBase64(file: File) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        if (typeof reader.result === "string") {
-          resolve(reader.result); // Ensure reader.result is a string
-        } else {
-          reject(new Error("Failed to convert file to Base64."));
+    function getImageBase64(file: File) {
+        return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            if (typeof reader.result === "string") {
+            resolve(reader.result); // Ensure reader.result is a string
+            } else {
+            reject(new Error("Failed to convert file to Base64."));
+            }
+        };
+        reader.onerror = reject;
+        reader.readAsDataURL(file); // Converts the file to Base64
+        });
+    }
+
+    // Function to hash the image
+    async function hashImage(file: File) {
+        try {
+        const base64Image = (await getImageBase64(file)) as string;
+
+        // Hash the Base64 string
+        const hash = CryptoJS.SHA256(base64Image).toString(CryptoJS.enc.Hex);
+        return hash;
+        } catch (error) {
+        console.error("Error hashing image:", error);
         }
-      };
-      reader.onerror = reject;
-      reader.readAsDataURL(file); // Converts the file to Base64
-    });
-  }
-
-  // Function to hash the image
-  async function hashImage(file: File) {
-    try {
-      const base64Image = (await getImageBase64(file)) as string;
-
-      // Hash the Base64 string
-      const hash = CryptoJS.SHA256(base64Image).toString(CryptoJS.enc.Hex);
-      return hash;
-    } catch (error) {
-      console.error("Error hashing image:", error);
     }
-  }
 
-  async function uploadImageToS3(signedUrl: string, file: File) {
-    try {
-      const response = await axios.put(signedUrl, file, {
-        headers: {
-          "Content-Type": file.type,
-        },
-      });
-
-      if (response.status === 200) {
-        Logger.log("Image uploaded successfully");
-      } else {
-        console.error("Failed to upload image:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Error uploading image to S3:", error);
-    }
-  }
-
-  // Function to handle file upload
-  const handleAddCard = async () => {
-    if (!selectedFile) return;
-    try {
-      setIsLoading(true);
-      const hashedImg = await hashImage(selectedFile);
-
-      if (hashedImg) {
-        const response = await uploadFile({
-          key: hashedImg,
-          type: selectedFile.type,
+    async function uploadImageToS3(signedUrl: string, file: File) {
+        try {
+        const response = await axios.put(signedUrl, file, {
+            headers: {
+            "Content-Type": file.type,
+            },
         });
-        await uploadImageToS3(response.signedUrl, selectedFile);
 
-        const content = await parseEvent({
-          srcUrl: response.srcUrl,
-          key: response.key,
-        });
-        const parsed = schoolEventSchema.parse(JSON.parse(content));
-        addEvent(parsed);
-      }
-    } catch (error) {
-      console.error("Error processing the file:", error);
-    } finally {
-      setSelectedFile(null);
-      setIsLoading(false);
+        if (response.status === 200) {
+            Logger.log("Image uploaded successfully");
+        } else {
+            console.error("Failed to upload image:", response.statusText);
+        }
+        } catch (error) {
+        console.error("Error uploading image to S3:", error);
+        }
     }
-  };
 
-  return (
-    <div style={{ padding: PADDING }} className="space-y-4">
-      <h1 className="text-2xl font-bold">Beyond Studies</h1>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="picture">Upload Image</Label>
-        <Input
-          id="picture"
-          type="file"
-          accept="image/*"
-          onChange={handleFileChange}
-        />
-      </div>
-      <Button
-        onClick={handleAddCard}
-        className="mb-4"
-        disabled={isLoading || !selectedFile}
-      >
-        {isLoading && <Loader2 className="mr-2 animate-spin" />} Add Event
-        Details
-      </Button>
+    // Function to handle file upload
+    const handleAddCard = async () => {
+        if (!selectedFile) return;
+        try {
+        setIsLoading(true);
+        const hashedImg = await hashImage(selectedFile);
 
-      {/* Starred Events Section */}
-      <div
-        className={cn(
-          "flex flex-col overflow-hidden rounded-lg bg-muted shadow-md",
-          !isMobile && "mb-6 mr-6 w-full flex-shrink-0"
-        )}
-      >
-        <div
-          className={cn(
-            "flex h-14 items-center justify-between bg-smu-blue p-3",
-            isMobile && "cursor-pointer"
-          )}
-        >
-          <h2 className="text-lg font-semibold text-primary-foreground">
-            Your Starred Events
-          </h2>
-          {isMobile && <ChevronDown className="text-primary-foreground" />}
-        </div>
+        if (hashedImg) {
+            const response = await uploadFile({
+            key: hashedImg,
+            type: selectedFile.type,
+            });
+            await uploadImageToS3(response.signedUrl, selectedFile);
 
-        <EventTabs
-          tabsData={{
-            upcoming: events.filter(
-              (event) => new Date(event.startTime) > new Date()
-            ),
-            past: events.filter(
-              (event) => new Date(event.endTime) < new Date()
-            ),
-          }}
-          eventCardActions={[
-            (event, index) => (
-              <Button
-                key={index}
-                onClick={() =>
-                  atcb_action({
-                    name: event.title,
-                    description: event.description,
-                    startDate: format(new Date(event.startTime), "yyyy-MM-dd"),
-                    startTime: format(new Date(event.startTime), "HH:mm"),
-                    endTime: format(new Date(event.endTime), "HH:mm"),
-                    location: event.venue,
-                    options: ["Google", "Outlook.com", "iCal", "Apple"],
-                    listStyle: "modal",
-                    lightMode: "bodyScheme",
-                  })
-                }
-              >
-                <Calendar className="mr-2" />
-                Add to Calendar
-              </Button>
-            ),
-            (event, index) => (
-              <Button
-                onClick={() => event.id && removeEvent(event.id)}
-                key={index}
-                variant={"destructive"}
-                size={"icon"}
-              >
-                <Trash />
-              </Button>
-            ),
-          ]}
-        />
-      </div>
+            const content = await parseEvent({
+            srcUrl: response.srcUrl,
+            key: response.key,
+            });
+            const parsed = schoolEventSchema.parse(JSON.parse(content));
+            addEvent(parsed);
+        }
+        } catch (error) {
+        console.error("Error processing the file:", error);
+        } finally {
+        setSelectedFile(null);
+        setIsLoading(false);
+        }
+    };
 
-      {/* Available Events Section */}
-      <div
-        className={cn(
-            "flex flex-col overflow-hidden rounded-lg bg-muted shadow-md ",
-            !isMobile && "mb-6 mr-6 w-full flex-shrink-0"
-        )}
-        >
-        <div
-            className={cn(
-            "flex h-14 items-center justify-between bg-smu-blue p-3",
-            isMobile && "cursor-pointer"
-            )}
-        >
-            <h2 className="text-lg font-semibold text-primary-foreground">
-            Available Events
-            </h2>
-            {isMobile && <ChevronDown className="text-primary-foreground" />}
-        </div>
-
-
-        <div className="p-4">
-            <EventTabs
-                tabsData={eventsData}
-                eventCardActions={[
-                (event, index) => (
-                    <Button onClick={() => addEvent(event)} key={index}>
-                    <Star className="mr-2" />
-                    Star
-                    </Button>
-                ),
-                ]}
+    return (
+        <div style={{ padding: PADDING }} className="space-y-4">
+        <h1 className="text-2xl font-bold">Beyond Studies</h1>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="picture">Upload Image</Label>
+            <Input
+            id="picture"
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
             />
         </div>
+        <Button
+            onClick={handleAddCard}
+            className="mb-4"
+            disabled={isLoading || !selectedFile}
+        >
+            {isLoading && <Loader2 className="mr-2 animate-spin" />} Add Event
+            Details
+        </Button>
+
+        {/* Starred Events Section */}
+        <div
+            className={cn(
+            "flex flex-col overflow-hidden rounded-lg bg-muted shadow-md",
+            !isMobile && "mb-6 mr-6 w-full flex-shrink-0"
+            )}
+        >
+            <div
+            className={cn(
+                "flex h-14 items-center justify-between bg-smu-blue p-3",
+                isMobile && "cursor-pointer"
+            )}
+            >
+            <h2 className="text-lg font-semibold text-primary-foreground">
+                Your Starred Events
+            </h2>
+            {isMobile && <ChevronDown className="text-primary-foreground"/>}
+            </div>
+
+            <div className="p-4">
+                <EventTabs
+                tabsData={{
+                    upcoming: events.filter(
+                    (event) => new Date(event.startTime) > new Date()
+                    ),
+                    past: events.filter(
+                    (event) => new Date(event.endTime) < new Date()
+                    ),
+                }}
+                eventCardActions={[
+                    (event, index) => (
+                    <Button
+                        key={index}
+                        onClick={() =>
+                        atcb_action({
+                            name: event.title,
+                            description: event.description,
+                            startDate: format(new Date(event.startTime), "yyyy-MM-dd"),
+                            startTime: format(new Date(event.startTime), "HH:mm"),
+                            endTime: format(new Date(event.endTime), "HH:mm"),
+                            location: event.venue,
+                            options: ["Google", "Outlook.com", "iCal", "Apple"],
+                            listStyle: "modal",
+                            lightMode: "bodyScheme",
+                        })
+                        }
+                    >
+                        <Calendar className="mr-2" />
+                        Add to Calendar
+                    </Button>
+                    ),
+                    (event, index) => (
+                    <Button
+                        onClick={() => event.id && removeEvent(event.id)}
+                        key={index}
+                        variant={"destructive"}
+                        size={"icon"}
+                    >
+                        <Trash />
+                    </Button>
+                    ),
+                ]}
+                />
+            </div>
         </div>
-    </div>
-  );
-}
+
+        {/* Available Events Section */}
+        <div
+            className={cn(
+                "flex flex-col overflow-hidden rounded-lg bg-muted shadow-md ",
+                !isMobile && "mb-6 mr-6 w-full flex-shrink-0"
+            )}
+            >
+            <div
+                className={cn(
+                "flex h-14 items-center justify-between bg-smu-gold p-3",
+                isMobile && "cursor-pointer"
+                )}
+            >
+                <h2 className="text-lg font-semibold text-primary-foreground">
+                Available Events
+                </h2>
+                {isMobile && <ChevronDown className="text-primary-foreground" />}
+            </div>
+
+
+            <div className="p-4">
+                <EventTabs
+                    tabsData={eventsData}
+                    eventCardActions={[
+                    (event, index) => (
+                        <Button onClick={() => addEvent(event)} key={index}>
+                        <Star className="mr-2" />
+                        Star
+                        </Button>
+                    ),
+                    ]}
+                />
+            </div>
+            </div>
+        </div>
+    );
+    }
