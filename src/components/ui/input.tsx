ed48711d8995cc258ef5c6c0855f112c96dc72ca
Isaiah Chia <130481550+Isaidchia@users.noreply.github.com>
2024-10-29 17:26:50 +0800

    implemented Search Bar

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -16,6 +16,8 @@ const inputVariants = cva(
         default: "border-input focus-visible:ring-ring",
         error: "border-red-500 focus-visible:ring-red-500",
         timetable: "border-input focus-visible:ring-blue-500",
+        beyondStudies:
+          "placeholder:text-white-foreground border-white focus-visible:ring-blue-500",
       },
     },
     defaultVariants: {
